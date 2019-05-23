329c20e9185816965f7b3342c589057ccb82605d
Andras Zsamboki <azsamboki@hiya.com>
2019-05-23 22:33:56 +0200

    reformat code

--- src/main/scala/jp/ne/opt/redshiftfake/s3/S3Service.scala
+++ src/main/scala/jp/ne/opt/redshiftfake/s3/S3Service.scala
@@ -43,8 +43,8 @@ class S3ServiceImpl(endpoint: String) extends S3Service {
     val client = credentials match {
       case Credentials.WithKey(accessKeyId, secretAccessKey) =>
         new AmazonS3Client(new BasicAWSCredentials(accessKeyId, secretAccessKey))
-      case Credentials.WithRole(roleName) =>
 
+      case Credentials.WithRole(roleName) =>
         val sts = AWSSecurityTokenServiceClientBuilder.standard.build
         val assumeRoleRequest = new AssumeRoleRequest
         assumeRoleRequest.withRoleArn(roleName)
